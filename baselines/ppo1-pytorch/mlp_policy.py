9cf7a6638e39a57eae839c28664b60f5f483c187
bjohnson <bkj.322@gmail.com>
2017-10-09 20:19:37 -0400

    edits

---
+++ baselines/ppo1-pytorch/mlp_policy.py
@@ -0,0 +1,151 @@
+#!/usr/bin/env python
+
+"""
+    mlp_policy.py
+"""
+
+import gym
+import numpy as np
+import tensorflow as tf
+import baselines.common.tf_util as U
+
+from baselines.common.mpi_running_mean_std import RunningMeanStd
+
+# --
+# Probability distributions
+
+class DiagGaussianPdType(object):
+    def __init__(self, size):
+        self.size = size
+    
+    def pdclass(self):
+        return DiagGaussianPd
+    
+    def param_shape(self):
+        return [2*self.size]
+    
+    def sample_shape(self):
+        return [self.size]
+    
+    def sample_dtype(self):
+        return tf.float32
+        
+    def pdfromflat(self, flat):
+        return self.pdclass()(flat)
+        
+    def param_placeholder(self, prepend_shape, name=None):
+        return tf.placeholder(dtype=tf.float32, shape=prepend_shape+self.param_shape(), name=name)
+    
+    def sample_placeholder(self, prepend_shape, name=None):
+        return tf.placeholder(dtype=self.sample_dtype(), shape=prepend_shape+self.sample_shape(), name=name)
+
+
+class DiagGaussianPd(object):
+    def __init__(self, flat):
+        self.flat = flat
+        mean, logstd = tf.split(axis=len(flat.shape)-1, num_or_size_splits=2, value=flat)
+        self.mean = mean
+        self.logstd = logstd
+        self.std = tf.exp(logstd)
+    
+    # @classmethod
+    # def fromflat(cls, flat):
+    #     return cls(flat)
+    
+    # def flatparam(self):
+    #     return self.flat
+    
+    def mode(self):
+        return self.mean
+    
+    # def kl(self, other):
+    #     assert isinstance(other, DiagGaussianPd)
+    #     return U.sum(other.logstd - self.logstd + (tf.square(self.std) + tf.square(self.mean - other.mean)) / (2.0 * tf.square(other.std)) - 0.5, axis=-1)
+    
+    def entropy(self):
+        return U.sum(self.logstd + .5 * np.log(2.0 * np.pi * np.e), axis=-1)
+    
+    def sample(self):
+        return self.mean + self.std * tf.random_normal(tf.shape(self.mean))
+    
+    def logp(self, x):
+        return - 0.5 * U.sum(tf.square((x - self.mean) / self.std), axis=-1) \
+               + 0.5 * np.log(2.0 * np.pi) * tf.to_float(tf.shape(x)[-1]) \
+               + U.sum(self.logstd, axis=-1)
+
+
+# --
+# Policy
+
+class MlpPolicy(object):
+    recurrent = False
+    def __init__(self, name, *args, **kwargs):
+        with tf.variable_scope(name):
+            self._init(*args, **kwargs)
+            self.scope = tf.get_variable_scope().name
+            
+    def _init(self, ob_space, ac_space, hid_size, num_hid_layers, gaussian_fixed_var=True):
+        assert isinstance(ob_space, gym.spaces.Box)
+        assert isinstance(ac_space, gym.spaces.Box)
+        
+        # Action space probability distribution
+        self.pdtype = pdtype = DiagGaussianPdType(ac_space.shape[0])
+        sequence_length = None
+        
+        # Observation
+        ob = U.get_placeholder(name="ob", dtype=tf.float32, shape=[sequence_length] + list(ob_space.shape))
+        
+        # runmean of ob
+        with tf.variable_scope("obfilter"):
+            self.ob_rms = RunningMeanStd(shape=ob_space.shape)
+        
+        # % difference from runmean
+        obz = tf.clip_by_value((ob - self.ob_rms.mean) / self.ob_rms.std, -5.0, 5.0)
+        
+        # Define MLP value function -- x_(i+1) = tanh(dense(x_i))
+        last_out = obz
+        for i in range(num_hid_layers):
+            last_out = tf.nn.tanh(U.dense(last_out, hid_size, "vffc%i"%(i+1), weight_init=U.normc_initializer(1.0)))
+        
+        self.vpred = U.dense(last_out, 1, "vffinal", weight_init=U.normc_initializer(1.0))[:,0]
+        
+        # Define MLP policy function
+        last_out = obz
+        for i in range(num_hid_layers):
+            last_out = tf.nn.tanh(U.dense(last_out, hid_size, "polfc%i"%(i+1), weight_init=U.normc_initializer(1.0)))
+        
+        # ??
+        if gaussian_fixed_var and isinstance(ac_space, gym.spaces.Box):
+            mean = U.dense(last_out, pdtype.param_shape()[0] // 2, "polfinal", U.normc_initializer(0.01))
+            logstd = tf.get_variable(name="logstd", shape=[1, pdtype.param_shape()[0] // 2], initializer=tf.zeros_initializer())
+            pdparam = U.concatenate([mean, mean * 0.0 + logstd], axis=1)
+        else:
+            pdparam = U.dense(last_out, pdtype.param_shape()[0], "polfinal", U.normc_initializer(0.01))
+        
+        # Convert weights back to "policy"
+        self.pd = pdtype.pdfromflat(pdparam)
+        
+        # Appear not to be used
+        self.state_in = []
+        self.state_out = []
+        
+        # Whether or not to sample from policy or take maximum
+        stochastic = tf.placeholder(dtype=tf.bool, shape=())
+        ac = U.switch(stochastic, self.pd.sample(), self.pd.mode())
+        
+        # Run
+        self._act = U.function([stochastic, ob], [ac, self.vpred])
+        
+    def act(self, stochastic, ob):
+        ac1, vpred1 =  self._act(stochastic, ob[None])
+        return ac1[0], vpred1[0]
+    
+    def get_variables(self):
+        return tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, self.scope)
+    
+    def get_trainable_variables(self):
+        return tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, self.scope)
+    
+    def get_initial_state(self):
+        return []
+
--- baselines/ppo1-pytorch/run_mujoco.py
+++ baselines/ppo1-pytorch/run_mujoco.py
@@ -1,19 +1,33 @@
-#!/usr/bin/env
+#!/usr/bin/env python
 
 """
-    pposgd_simple.py
+    run_mujoco.py
 """
 
+import sys
+import gym
 import time
+import logging
+import argparse
 import numpy as np
 import tensorflow as tf
 from collections import deque
 
-from baselines import logger
+from baselines import bench, logger
 import baselines.common.tf_util as U
-from baselines.common import Dataset, zipsame
-
 from baselines.common.mpi_adam import MpiAdam
+from baselines.common import set_global_seeds, Dataset, zipsame
+
+
+import mlp_policy
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--env', default='Hopper-v1')
+    parser.add_argument('--seed', type=int, default=0)
+    parser.add_argument('--num-timesteps', type=int, default=int(1e6))
+    return parser.parse_args()
+
 
 # --
 # Helpers
@@ -21,10 +35,8 @@ from baselines.common.mpi_adam import MpiAdam
 def flatten_lists(listoflists):
     return [el for list_ in listoflists for el in list_]
 
-# --
-# Segment generator
 
-def make_segment_generator(pi, env, horizon, stochastic, gamma, lam):
+def make_segment_generator(pi, env, horizon, stochastic):
     t = 0
     ac = env.action_space.sample() # not used, just so we have the datatype
     new = True # marks if we're on first timestep of an episode
@@ -41,48 +53,37 @@ def make_segment_generator(pi, env, horizon, stochastic, gamma, lam):
     vpreds  = np.zeros(horizon, 'float32')
     news    = np.zeros(horizon, 'int32')
     acs     = np.array([ac for _ in range(horizon)])
+    prevacs = acs.copy()
     
     while True:
+        prevac = ac
         ac, vpred = pi.act(stochastic, ob)
         # Slight weirdness here because we need value function at time T
         # before returning segment [0, T-1] so we get the correct
         # terminal value
         if t > 0 and t % horizon == 0:
-            
-            nextvpred = vpred * (1 - new)
-            
-            news   = np.append(news, 0) # last element is only used for last vtarg, but we already zeroed it if last new = 1
-            vpreds = np.append(vpreds, nextvpred)
-            atargs = np.zeros(len(rews) + 1, 'float32')
-            
-            for t in reversed(range(len(rews))):
-                nonterminal = 1 - news[t+1]
-                delta = rews[t] + gamma * vpreds[t+1] * nonterminal - vpreds[t]
-                atargs[t] = delta + gamma * lam * nonterminal * atargs[t+1]
-            
-            vpreds = vpreds[:-1]
-            atargs = atargs[:-1]
-            
             yield {
-                "obs"        : obs,
-                "acs"        : acs,
-                "vtargs"     : atargs + vpred,
-                "atargs"     : (atargs - atargs.mean()) / atargs.std(),
-                
-                "ep_rets"    : ep_rets,
-                "ep_lens"    : ep_lens,
+                "ob" : obs,
+                "rew" : rews,
+                "vpred" : vpreds,
+                "new" : news,
+                "ac" : acs,
+                "prevac" : prevacs,
+                "nextvpred" : vpred * (1 - new),
+                "ep_rets" : ep_rets,
+                "ep_lens" : ep_lens,
             }
-            
             # Be careful!!! if you change the downstream algorithm to aggregate
             # several of these batches, then be sure to do a deepcopy
             ep_rets = []
             ep_lens = []
         
-        i          = t % horizon
-        obs[i]     = ob
-        vpreds[i]  = vpred
-        news[i]    = new
-        acs[i]     = ac
+        i = t % horizon
+        obs[i] = ob
+        vpreds[i] = vpred
+        news[i] = new
+        acs[i] = ac
+        prevacs[i] = prevac
         
         ob, rew, new, _ = env.step(ac)
         rews[i] = rew
@@ -101,22 +102,49 @@ def make_segment_generator(pi, env, horizon, stochastic, gamma, lam):
 # --
 # Run
 
-def learn(env, policy_func, *,
-        timesteps_per_batch, # timesteps per actor per update
-        clip_param, entcoeff, # clipping parameter epsilon, entropy coeff
-        optim_epochs, optim_stepsize, optim_batchsize, # optimization hypers
-        gamma, lam, # advantage estimation
-        max_timesteps=0, max_episodes=0, max_iters=0, max_seconds=0, # time constraint
-        callback=None, # you can do anything in the callback, since it takes locals(), globals()
-        adam_epsilon=1e-5,
-        schedule='constant' # annealing for stepsize parameters (epsilon and adam)
-    ):
+if __name__ == '__main__':
+    args = parse_args()
     
-    # --
-    # Placeholders
+    U.make_session(num_cpu=1).__enter__()
+    set_global_seeds(args.seed)
+    env = gym.make(args.env)
     
-    pi = policy_func("pi", env.observation_space, env.action_space) # Construct network for new policy
-    oldpi = policy_func("oldpi", env.observation_space, env.action_space) # Network for old policy
+    def policy_func(name, ob_space, ac_space):
+        return 
+    
+    env = bench.Monitor(env, "monitor.json")
+    env.seed(args.seed)
+    gym.logger.setLevel(logging.WARN)
+    
+    # Set parameters
+    max_timesteps=args.num_timesteps
+    timesteps_per_batch=2048
+    clip_param=0.2
+    entcoeff=0.0
+    optim_epochs=10
+    optim_stepsize=3e-4
+    optim_batchsize=64
+    gamma=0.99
+    lam=0.95
+    schedule='linear'
+    adam_epsilon=1e-5
+    
+    # Policy networks
+    pi = mlp_policy.MlpPolicy(
+        name='pi',
+        ob_space=env.observation_space,
+        ac_space=env.action_space,
+        hid_size=64, 
+        num_hid_layers=2
+    )
+    
+    oldpi = mlp_policy.MlpPolicy(
+        name='oldpi',
+        ob_space=env.observation_space,
+        ac_space=env.action_space,
+        hid_size=64, 
+        num_hid_layers=2
+    )
     
     atarg = tf.placeholder(dtype=tf.float32, shape=[None]) # Target advantage function (if applicable)
     ret = tf.placeholder(dtype=tf.float32, shape=[None]) # Observed return
@@ -164,7 +192,7 @@ def learn(env, policy_func, *,
     # --
     # Initialize rollouts
     
-    segment_generator = make_segment_generator(pi, env, timesteps_per_batch, stochastic=True, gamma=gamma, lam=lam)
+    segment_generator = make_segment_generator(pi, env, timesteps_per_batch, stochastic=True)
     
     episodes_so_far  = 0
     timesteps_so_far = 0
@@ -174,24 +202,10 @@ def learn(env, policy_func, *,
     lenbuffer = deque(maxlen=100) # rolling buffer for episode lengths
     rewbuffer = deque(maxlen=100) # rolling buffer for episode rewards
     
-    assert sum([max_iters > 0, max_timesteps > 0, max_episodes > 0, max_seconds > 0]) == 1, "Only one time constraint permitted"
-    
     # --
     # Run
     
-    while True:
-        if callback:
-            callback(locals(), globals())
-        
-        # Stopping conditions
-        if max_timesteps and timesteps_so_far >= max_timesteps:
-            break
-        elif max_episodes and episodes_so_far >= max_episodes:
-            break
-        elif max_iters and iters_so_far >= max_iters:
-            break
-        elif max_seconds and time.time() - tstart >= max_seconds:
-            break
+    while timesteps_so_far < max_timesteps:
         
         # Learning rate decay
         if schedule == 'constant':
@@ -206,41 +220,54 @@ def learn(env, policy_func, *,
         # Sample rollouts
         seg = segment_generator.__next__()
         
+        # ?? Compute vtarg and atarg -- not sure what they are
+        new, vpred, rew = seg['new'], seg['vpred'], seg['rew']
+        new = np.append(new, 0) # last element is only used for last vtarg, but we already zeroed it if last new = 1
+        vpred = np.append(vpred, seg["nextvpred"])
+        atarg = np.zeros(len(rew) + 1, 'float32')
+        
+        for t in reversed(range(len(rew))):
+            nonterminal = 1 - new[t+1]
+            delta = rew[t] + gamma * vpred[t+1] * nonterminal - vpred[t]
+            atarg[t] = delta + gamma * lam * nonterminal * atarg[t+1]
+        
+        vpred = vpred[:-1]
+        atarg = atarg[:-1]
+        
+        # make dataset
+        dataset = Dataset({
+            "ob" : seg['ob'],
+            "ac" : seg['ac'],
+            "vtarg" : atarg + vpred,
+            "atarg" : (atarg - atarg.mean()) / atarg.std(),
+        }, shuffle=not pi.recurrent)
+        
         # update running mean/std for policy, if applicable
         if hasattr(pi, "ob_rms"):
-            pi.ob_rms.update(seg['obs'])
+            pi.ob_rms.update(seg['ob'])
         
          # set oldpi = pi
         update_oldpi()
         
         # Optimize
-        dataset = Dataset({
-            "obs"    : seg['obs'],
-            "acs"    : seg['acs'],
-            "vtargs" : seg['vtargs'],
-            "atargs" : seg['atargs'],
-        }, shuffle=not pi.recurrent)
-        
         for _ in range(optim_epochs):
             for batch in dataset.iterate_once(optim_batchsize or seg['ob'].shape[0]):
-                g = compute_grad(batch["obs"], batch["acs"], batch["atargs"], batch["vtargs"], cur_lrmult)
+                g = compute_grad(batch["ob"], batch["ac"], batch["atarg"], batch["vtarg"], cur_lrmult)
                 opt.update(g, optim_stepsize * cur_lrmult) 
         
         # Tracking steps + logging
-        ep_lens = seg["ep_lens"]
-        ep_rets = seg["ep_rets"]
-        
-        episodes_so_far += len(ep_lens)
-        timesteps_so_far += sum(ep_lens)
+        episodes_so_far += len(seg["ep_lens"])
+        timesteps_so_far += sum(seg["ep_lens"])
         iters_so_far += 1
         
-        lenbuffer.extend(ep_lens)
-        rewbuffer.extend(ep_rets)
+        lenbuffer.extend(seg["ep_lens"])
+        rewbuffer.extend(seg["ep_rets"])
         logger.record_tabular("EpLenMean", np.mean(lenbuffer))
         logger.record_tabular("EpRewMean", np.mean(rewbuffer))
-        logger.record_tabular("EpThisIter", len(ep_lens))
+        logger.record_tabular("EpThisIter", len(seg["ep_lens"]))
         logger.record_tabular("EpisodesSoFar", episodes_so_far)
         logger.record_tabular("TimestepsSoFar", timesteps_so_far)
         logger.record_tabular("TimeElapsed", time.time() - tstart)
-        if MPI.COMM_WORLD.Get_rank() == 0:
-            logger.dump_tabular()
+        logger.dump_tabular()
+    
+    env.close()
